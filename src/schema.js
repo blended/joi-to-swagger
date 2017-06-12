3c25acad358f554bf7bba3e540a7723180d5a90a
George Stagas <gstagas@gmail.com>
2017-06-12 10:17:47 +0200

    feat: use description

--- src/schema.js
+++ src/schema.js
@@ -6,15 +6,18 @@ export default function parseSchema(schema) {
     let result = {};
     if (schema._type === 'object') {
         result.type = 'object';
+        result.description = schema._description
         result.properties = {};
         _.each(schema._inner.children, (property) => {
             result.properties[property.key] = parser(property.schema);
         });
     } else if (schema._type === 'array') {
         result.type = 'array';
+        result.description = schema._description
         result.items = parseSchema(schema._inner.items[0]);
     } else if (_.isObject(schema) && !schema._type) {
         result.type = 'object';
+        result.description = schema._description
         result.properties = _.mapValues(schema, (value) => {
             if (_.isArray(value)) {
                 return {
@@ -27,22 +30,6 @@ export default function parseSchema(schema) {
         });
     } else {
         return parser(schema);
-    }
-
-    result.required = _.reduce(result.properties, (arr, value, key) => {
-        if (value.required) {
-            arr.push(key);
-        }
-
-        return arr;
-    }, []);
-
-    _.each(result.properties, (value, key) => {
-        delete result.properties[key].required;
-    });
-
-    if (result.required.length === 0) {
-        delete result.required;
     }
 
     return result;
